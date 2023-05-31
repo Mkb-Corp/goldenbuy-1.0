cb41334daa48aa36bd2ec054a11ba3a0bc7cb087
emmamkb <emmanuelmakabu7@gmail.com>
2023-05-31 13:55:19 +0200

    ✨ Add models

---
+++ app/Models/Category.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+
+class Category extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'details',
+        'picture',
+        'icon',
+    ];
+
+    public function sub_categories():HasMany
+    {
+        return $this->hasMany(SubCategory::class);
+    }
+
+}
